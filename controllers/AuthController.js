27178a41d310ac3178114757af21f9ec849a035d
mfcastilho <fredericocastilho@hotmail.com>
2023-03-30 03:27:08 -0300

    inserting at the store method the try/catch

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,27 +7,44 @@ const AuthController = {
 
      },
      store: async (req, res)=>{
-          const {name, email, nickname, password} = req.body;
-          const hashPassword = bcrypt.hashPassword(password, 10);
+          try {
 
-          const veriryIfUserExists = await User.findOne({
-               where:{
-                    [Op.or]:[{nickname}, {email}]
+               const {name, email, nickname, password} = req.body;
+               const hashPassword = bcrypt.hashPassword(password, 10);
+
+               const veriryIfUserExists = await User.findOne({
+                    where:{
+                         [Op.or]:[{nickname}, {email}]
+                    }
+               });
+
+               if(veriryIfUserExists){
+                    return res.status(400).json({error:true, message: "Não foi possível realizar o cadastro."});
                }
-          });
 
-          if(veriryIfUserExists){
-               return res.status(400).json({error:true, message: "Não foi possível realizar o cadastro."});
+               const newUser = await User.create({
+                    name,
+                    email,
+                    nickname,
+                    password: hashPassword
+               });
+
+               return res.status(201).json({data: newUser});
+
+          } catch (error) {
+               
+               if (error.name === "SequelizeConnectionRefusedError"){
+                    return res.status(500).json({error: true, message: "Sistema indisponível, tente novamente mais tarde!"})
+                  }
+            
+                  if (error.name === "SequelizeUniqueConstraintError"){
+                      return res.status(400).json(error.parent.sqlMessage);
+                  }
+            
+                  if (error.name === "SequelizeValidationError"){
+                      return res.status(400).json({error: true, message: `${error.errors[0].type} at ${error.errors[0].path}`})
+                  }
           }
-
-          const newUser = await User.create({
-               name,
-               email,
-               nickname,
-               password: hashPassword
-          });
-
-          return res.status(201).json({data: newUser});
      }     
 
 }
